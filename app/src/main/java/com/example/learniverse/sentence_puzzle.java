5a4d8d7dddac0d97e5b6d26dca300d17af83d26e
Ayumi <ayumi.xie5@gmail.com>
2024-03-17 21:13:06 +0700

    Sentence puzzle

---
+++ app/src/main/java/com/example/learniverse/sentence_puzzle.java
@@ -0,0 +1,13 @@
+package com.example.learniverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sentence_puzzle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sentence_puzzle);
+    }
+}
