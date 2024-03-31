58787a10d9320d8441bf72a26d5c4405cfd20263
pwhalenn <farencialevis19@gmail.com>
2024-04-01 01:54:27 +0700

    change gambar capital

--- app/src/main/java/com/example/learniverse/StoryTaleActivity.java
+++ app/src/main/java/com/example/learniverse/StoryTaleActivity.java
@@ -58,7 +58,7 @@ public class StoryTaleActivity extends AppCompatActivity {
 
         clearAllStory();
         //init data
-        simpanDataStory("Story Title", "Story desc...", R.drawable.Gambar1);
+        simpanDataStory("Story Title", "Story desc...", R.drawable.gambar1);
     }
 
     public void simpanDataStory(String namaStory, String descStory, int gambarStory){
