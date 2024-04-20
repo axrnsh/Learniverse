c76e1a06c5adfe4433c8cd54da8160abe7873e1d
pwhalenn <farencialevis19@gmail.com>
2024-04-21 00:56:58 +0700

    Adding Necessary File for Guessing Game

---
+++ app/src/main/java/com/example/learniverse/GuessingGameScoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.learniverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuessingGameScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guessing_game_score);
+    }
+}
