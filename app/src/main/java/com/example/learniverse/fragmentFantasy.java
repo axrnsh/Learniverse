bd8d96acca5bd4aeabd755a99ff7b0aa12afd9c9
pwhalenn <farencialevis19@gmail.com>
2024-04-21 23:45:31 +0700

    fragment error handle

--- app/src/main/java/com/example/learniverse/fragmentFantasy.java
+++ app/src/main/java/com/example/learniverse/fragmentFantasy.java
@@ -51,8 +51,8 @@ public class fragmentFantasy extends Fragment {
                 .deleteRealmIfMigrationNeeded()
                 .allowWritesOnUiThread(true)
                 .build();
-        Realm.setDefaultConfiguration(config);
 
+        Realm.setDefaultConfiguration(config);
         // Open the Realm instance
         realm = Realm.getDefaultInstance();
 
@@ -64,7 +64,7 @@ public class fragmentFantasy extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_education_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_fantasy_list, container, false);
 
         // Set up RecyclerView
         if (view instanceof RecyclerView) {
--- app/src/main/java/com/example/learniverse/fragmentPoetry.java
+++ app/src/main/java/com/example/learniverse/fragmentPoetry.java
@@ -64,7 +64,7 @@ public class fragmentPoetry extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_education_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_poetry_list, container, false);
 
         // Set up RecyclerView
         if (view instanceof RecyclerView) {
