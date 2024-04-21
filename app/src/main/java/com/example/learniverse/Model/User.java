76147485df209a5b50e7942eaa87fca05abd3cf9
pwhalenn <farencialevis19@gmail.com>
2024-04-21 12:17:36 +0700

    small update

--- app/src/main/java/com/example/learniverse/Model/User.java
+++ app/src/main/java/com/example/learniverse/Model/User.java
@@ -5,11 +5,9 @@ import io.realm.annotations.PrimaryKey;
 import io.realm.annotations.Required;
 
 public class User extends RealmObject {
-
     private long idUser;
     private String username;
     private String password;
-
     private Story idStory;
     private Card idCard;
     private HistoryQuiz historyQuiz;
