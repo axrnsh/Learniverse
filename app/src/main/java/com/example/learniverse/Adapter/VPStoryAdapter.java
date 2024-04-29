02b25133cb85ea1e9ac68e2a5324f81157f8796f
Ayumi <ayumi.xie5@gmail.com>
2024-04-29 11:46:17 +0700

    entah kenapa bisa wkwkkwk

--- app/src/main/java/com/example/learniverse/Adapter/VPStoryAdapter.java
+++ app/src/main/java/com/example/learniverse/Adapter/VPStoryAdapter.java
@@ -1,10 +1,14 @@
 package com.example.learniverse.Adapter;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.learniverse.Model.Story;
+import com.example.learniverse.ReadStoryActivity;
 import com.example.learniverse.databinding.ListStoryLayoutBinding;
 import com.example.learniverse.placeholder.PlaceholderContent;
 
@@ -30,6 +34,17 @@ public class VPStoryAdapter extends RecyclerView.Adapter<VPStoryAdapter.ViewHold
         holder.binding.imageStory.setImageResource(story.getGambarStory());
         holder.binding.txtStoryTitle.setText(story.getNamaStory());
         holder.binding.txtStoryDesc.setText(story.getDescStory());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Story clickedStory = mStories.get(holder.getAdapterPosition());
+                if (clickedStory.getNamaStory().equals("Harry Potter and the Sorcerer's Stone")) {
+                    Intent intent = new Intent(view.getContext(), ReadStoryActivity.class);
+                    view.getContext().startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
